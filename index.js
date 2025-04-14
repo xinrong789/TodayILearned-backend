903ace5a77833ee785ea083157b7fe9e9c2d08f1
xinrong789 <xinronglee@126.com>
2025-04-15 00:08:56 +0800

    updata data source

--- index.js
+++ index.js
@@ -11,11 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 // connect to mysql
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "studyinfo",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+//read data from render
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "studyinfo",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
